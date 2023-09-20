f0ae4063efaf779959ce8237ba480f1a024708be
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-09-20 20:17:01 +0600

    all done

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,12 +1,18 @@
 import { Schema, model } from "mongoose";
 import { IUser, UserModel } from "./user.interface";
 
-const user_schema = new Schema<IUser>({
-  name: { type: String },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  pc_building: [{ type: Schema.Types.Mixed }],
-});
+const user_schema = new Schema<IUser>(
+  {
+    name: { type: String },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    pc_building: [{ type: Schema.Types.Mixed }],
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+  }
+);
 
 const User: UserModel = model<IUser>("User", user_schema);
 
